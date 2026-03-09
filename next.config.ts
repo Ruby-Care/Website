09ae7972674e814384dee45721565b0559d23ca4
Bart <bart@Studio.local>
2026-03-09 13:20:44 +0100

    added configurabel local domains

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['r-web-local.test'],
+};
 
 export default withNextIntl(nextConfig);
